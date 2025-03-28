7697b1a5aaf60a46d7db68e650b236d3b34905f7
mikeheul <mickael.murmann@gmail.com>
2025-03-28 16:07:34 +0100

    Update user retrieval in PostController and enhance password hashing in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,23 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Post;
-use App\Entity\Comment;
-use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Entity\Comment;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR'); 
@@ -19,8 +27,13 @@ class AppFixtures extends Fixture
         for ($i = 1; $i <= 5; $i++) {
             $user = new User();
             $user->setPseudo($faker->userName)
-                ->setPassword($faker->password) 
+                // ->setPassword($faker->password) 
                 ->setEmail($faker->safeEmail);
+
+            $plainPassword = 'password123'; 
+
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
 
             $manager->persist($user);
             $users[] = $user; 
